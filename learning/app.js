81d50199ba54e3504f68a88b9cc38c11d264ef7b
RAJESHPOOSAPATI <rajesh.57102@gmail.com>
2015-08-03 11:29:12 +0530

    Commit Message

---
+++ learning/app.js
@@ -0,0 +1,9 @@
+var ViewModel = function(){
+	this.ClickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imgAttribution = ko.observable('https://www.flikr.com/phoos/big');
+
+	this.incrementCounter = function() {};
+}
+ko.applyBuildings(new ViewModel());
