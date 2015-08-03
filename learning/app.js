14858033c2b8693361177290ddd147dea51b6018
RAJESHPOOSAPATI <rajesh.57102@gmail.com>
2015-08-03 13:18:06 +0530

    adding semicolon

--- learning/app.js
+++ learning/app.js
@@ -4,6 +4,8 @@ var ViewModel = function(){
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flikr.com/phoos/big');
 
-	this.incrementCounter = function() ;
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
 }
 ko.applyBuildings(new ViewModel());
